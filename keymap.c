dd54e162b8e1062291c84d807f2bf3b2beadd541
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2022-02-21 11:57:16 +0100

    Made mac compatible

--- keymap.c
+++ keymap.c
@@ -7,6 +7,11 @@ const key_override_t vi_down_override = ko_make_basic(MOD_MASK_ALT, KC_J, KC_DOW
 const key_override_t vi_up_override = ko_make_basic(MOD_MASK_ALT, KC_K, KC_UP);
 const key_override_t vi_right_override = ko_make_basic(MOD_MASK_ALT, KC_L, KC_RIGHT);
 
+const key_override_t vi_home_override = ko_make_basic(MOD_MASK_ALT, KC_Y, G(KC_LEFT));
+const key_override_t vi_pgdn_override = ko_make_basic(MOD_MASK_ALT, KC_U, G(KC_DOWN));
+const key_override_t vi_pgup_override = ko_make_basic(MOD_MASK_ALT, KC_I, G(KC_UP));
+const key_override_t vi_end_override = ko_make_basic(MOD_MASK_ALT, KC_O, G(KC_RIGHT));
+
 // Shift + esc = ~
 const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRAVE));
 
@@ -18,6 +23,12 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &vi_down_override,
     &vi_up_override,
     &vi_right_override,
+
+    &vi_end_override,
+    &vi_home_override,
+    &vi_pgup_override,
+    &vi_pgdn_override,
+
     &tilde_esc_override,
     &grave_esc_override,
     NULL // Null terminate the array of overrides!
@@ -33,7 +44,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_65_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, TG(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_UP, KC_END, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, TO(0), TO(1), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT),
   [1] = LAYOUT_65_ansi(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_SPI, RGB_RMOD, RGB_MOD, RGB_TOG, KC_TRNS, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT)
 };
-
 
 /**
  * Change ctl+backspace into delete and do not register the alt modifier.
