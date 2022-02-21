c75bd4ddf4e44443461f2c0a3a673339d7de28d4
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2022-02-21 09:18:04 +0100

    WIP

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 #pragma once
 
-#define TAPPING_TERM 175
-#define COMBO_COUNT 1
+#define COMBO_COUNT 0
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
--- keymap.c
+++ keymap.c
@@ -1,31 +1,26 @@
 #include QMK_KEYBOARD_H
 
-/* combo */
+//  enable vi style movement
 
 const key_override_t vi_left_override = ko_make_basic(MOD_MASK_ALT, KC_H, KC_LEFT);
 const key_override_t vi_down_override = ko_make_basic(MOD_MASK_ALT, KC_J, KC_DOWN);
 const key_override_t vi_up_override = ko_make_basic(MOD_MASK_ALT, KC_K, KC_UP);
 const key_override_t vi_right_override = ko_make_basic(MOD_MASK_ALT, KC_L, KC_RIGHT);
 
+// Shift + esc = ~
+const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRAVE));
+
+// GUI + esc = `
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_GUI, KC_ESC, KC_GRAVE);
+
 const key_override_t **key_overrides = (const key_override_t *[]){
     &vi_left_override,
     &vi_down_override,
     &vi_up_override,
     &vi_right_override,
+    &tilde_esc_override,
+    &grave_esc_override,
     NULL // Null terminate the array of overrides!
-};
-
-/* tapdance */
-
-// Tap Dance declarations
-enum {
-    TD_LSFT_TILDE,
-};
-
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for shift, twice for tilde
-    [TD_LSFT_TILDE] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_TILDE),
 };
 
 /* THIS FILE WAS GENERATED!
@@ -35,13 +30,13 @@ qk_tap_dance_action_t tap_dance_actions[] = {
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_65_ansi(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, TD(TD_LSFT_TILDE), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_END, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, TO(0), MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
+	[0] = LAYOUT_65_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, TD(TD_LSFT_TILDE), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_END, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, TO(0), MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
 	[1] = LAYOUT_65_ansi(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2, KC_BTN1, KC_TRNS, KC_LSFT, KC_WH_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_SPI, RGB_RMOD, RGB_MOD, RGB_TOG, KC_TRNS, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT)
 };
 
 
 /**
- * Change ctrl+backspace into delete and do not register the ctrl modifier.
+ * Change ctl+backspace into delete and do not register the alt modifier.
  */
 static bool control_disabled = false;
 static bool delete_pressed = false;
